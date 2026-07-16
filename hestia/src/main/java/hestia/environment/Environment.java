7c486f7556b6ce1193772b5130874d6d9c9311b9
Marcus Warm <marcus.warm@x-map.de>
2026-07-16 08:59:54 +0200

    environment

--- hestia/src/main/java/hestia/environment/Environment.java
+++ hestia/src/main/java/hestia/environment/Environment.java
@@ -2,6 +2,7 @@ package hestia.environment;
 
 public class Environment {
     private String name;
+    private boolean active = true;
 
     public String getName() {
         return name;
@@ -9,5 +10,13 @@ public class Environment {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 }
