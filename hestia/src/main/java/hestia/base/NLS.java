6dce9da13b473a972192be438504eb7178a23a14
Marcus Warm <marcus.warm@x-map.de>
2026-07-23 11:59:42 +0200

    encoding

--- hestia/src/main/java/hestia/base/NLS.java
+++ hestia/src/main/java/hestia/base/NLS.java
@@ -1,8 +1,13 @@
 package hestia.base;
 
 import static github.soltaufintel.amalia.web.action.NLSService.loadDataMap;
-import static github.soltaufintel.amalia.web.action.NLSService.loadRB;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Map;
 
 import com.github.template72.data.DataMap;
@@ -11,8 +16,8 @@ import com.github.template72.data.DataMap;
  * National language support
  */
 public class NLS {
-    public static final Map<String, String> de = loadRB("de", "/de.rb", NLS.class);
-    public static final Map<String, String> en = loadRB("en", "/en.rb", NLS.class);
+    public static final Map<String, String> de = loadRB("de", "/de.rb", NLS.class, StandardCharsets.UTF_8);
+    public static final Map<String, String> en = loadRB("en", "/en.rb", NLS.class, StandardCharsets.UTF_8);
     public static DataMap dataMap_de = loadDataMap(de);
     public static DataMap dataMap_en = loadDataMap(en);
     
@@ -26,5 +31,28 @@ public class NLS {
     public static String get(String lang, String key) {
         String text = getProperties(lang).get(key);
         return text == null ? key : text;
+    }
+    
+    // TODO amalia: charset
+    public static Map<String, String> loadRB(String language, String filename, Class<?> cls, Charset charset) {
+        // Properties Klasse ist doof
+        Map<String, String> map = new HashMap<>();
+        try (BufferedReader r = new BufferedReader(new InputStreamReader(cls.getResourceAsStream(filename), charset))) {
+            String line;
+            while ((line = r.readLine()) != null) {
+                if (line.trim().startsWith("#")) {
+                    continue;
+                }
+                int o = line.indexOf("=");
+                if (o >= 0) {
+                    String key = line.substring(0, o).trim();
+                    String value = line.substring(o + 1).trim();
+                    map.put(key, value);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading NLS file '" + filename + "' for language '" + language + "'", e);
+        }
+        return map;
     }
 }
