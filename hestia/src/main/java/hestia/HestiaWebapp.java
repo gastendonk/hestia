389119e50d17c94e2015672387c10362df1d62ce
Marcus Warm <marcus.warm@x-map.de>
2026-08-20 15:48:56 +0200

    TODO

--- hestia/src/main/java/hestia/HestiaWebapp.java
+++ hestia/src/main/java/hestia/HestiaWebapp.java
@@ -49,6 +49,7 @@ import hestia.web.base.HestiaErrorPage;
 import hestia.web.base.HestiaPageInitializer;
 import spark.Spark;
 
+// TODO Wie kann ich das OTC Log einsehen?
 public class HestiaWebapp extends RouteDefinitions {
     public static final String VERSION = "0.1.0";
     public static HestiaConfig config;
