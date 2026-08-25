ead0be6a1dfea7227e3ffe7864a7fa3a16a79691
Marcus Warm <marcus.warm@x-map.de>
2026-08-25 10:21:56 +0200

    non blocking logging

--- hestia/src/main/java/hestia/otc/OtcLog.java
+++ hestia/src/main/java/hestia/otc/OtcLog.java
@@ -2,6 +2,8 @@ package hestia.otc;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
@@ -9,25 +11,36 @@ import java.util.logging.SimpleFormatter;
 import github.soltaufintel.amalia.base.FileService;
 
 public class OtcLog {
+    private static final int QUEUE_SIZE = 10_000;
+    private static final BlockingQueue<String> queue = new ArrayBlockingQueue<>(QUEUE_SIZE);
     private static File file;
     private static Logger logger;
-
+    private static FileHandler fileHandler;
+    private static Thread writerThread;
+    
     private OtcLog() {
     }
     
-    public static void init() {
+    public static synchronized void init() {
         final int mb = 2;
+        final int bytesLimit = mb * 1024 * 1024;
+        
         file = new File(System.getProperty("java.io.tmpdir"), "otc.log");
+        stopWriter();
 
         logger = Logger.getLogger("OTelCollectorFileLogger");
         logger.setUseParentHandlers(false); // VERHINDERT Konsolen- / Docker-Ausgabe!
 
+        for (var handler : logger.getHandlers()) {
+            logger.removeHandler(handler);
+            handler.close();
+        }
+        queue.clear();
+        
         try {
-            int bytesLimit = mb * 1024 * 1024;
-
             // FileHandler(path, limitBytes, count, append)
             // count = 1 -> Es existiert STRENG GENAU 1 Datei
-            FileHandler fileHandler = new FileHandler(file.getAbsolutePath(), bytesLimit, 1, false);
+            fileHandler = new FileHandler(file.getAbsolutePath(), bytesLimit, 1, false);
 
             // Schlankes Format: Nur Uhrzeit + Nachricht
             fileHandler.setFormatter(new SimpleFormatter() {
@@ -39,14 +52,41 @@ public class OtcLog {
 
             logger.addHandler(fileHandler);
         } catch (IOException e) {
-            org.pmw.tinylog.Logger.error(e, "Konnte OTel FileHandler nicht initialisieren");
+            org.pmw.tinylog.Logger.error(e, "Could not init OTC log file");
         }
+        writerThread = new Thread(OtcLog::writeLoop);
+        writerThread.setName("OTC-LogWriter");
+        writerThread.setDaemon(true);
+        writerThread.start();
     }
 
     public static void info(String line) {
-        logger.info(line);
+        queue.offer(line);
     }
-    
+
+    private static void writeLoop() {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                String line = queue.take();
+                logger.info(line);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private static void stopWriter() {
+        if (writerThread != null) {
+            writerThread.interrupt();
+            writerThread = null;
+        }
+        if (fileHandler != null) {
+            fileHandler.flush();
+            fileHandler.close();
+            fileHandler = null;
+        }
+    }
+
     public static String load() {
         return FileService.loadPlainTextFile(file);
     }
