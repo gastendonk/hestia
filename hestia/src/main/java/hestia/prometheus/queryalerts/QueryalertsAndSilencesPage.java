6d8cd51fa08c0e9854fc43d906cc2e487e7ac518
Marcus Warm <marcus.warm@x-map.de>
2026-09-03 14:33:03 +0200

    qas: Alert rule link

--- hestia/src/main/java/hestia/prometheus/queryalerts/QueryalertsAndSilencesPage.java
+++ hestia/src/main/java/hestia/prometheus/queryalerts/QueryalertsAndSilencesPage.java
@@ -3,6 +3,8 @@ package hestia.prometheus.queryalerts;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.pmw.tinylog.Logger;
 
@@ -11,13 +13,17 @@ import github.soltaufintel.amalia.web.table.Cols;
 import github.soltaufintel.amalia.web.table.TableComponent;
 import hestia.HestiaWebapp;
 import hestia.config.HestiaConfig;
+import hestia.environment.Environment;
+import hestia.prometheus.alert.AlertGroup;
+import hestia.prometheus.alert.rule.AlertRule;
 import hestia.prometheus.silences.PrometheusSilencesService;
 import hestia.prometheus.silences.Silence;
 import hestia.web.base.HPage;
 
 public class QueryalertsAndSilencesPage extends HPage {
     // TODO extend silence
-
+    private Map<String, List<AlertGroup>> groups;
+    
     @Override
     protected void execute() {
         HestiaConfig c = HestiaWebapp.config;
@@ -35,6 +41,11 @@ public class QueryalertsAndSilencesPage extends HPage {
             Logger.error(e.getMessage());
             silences = new ArrayList<>();
         }
+        // load all alert groups
+        var groupDAO = c.alertGroupDAO(b());
+        for (Environment env : c.environmentDAO(b()).load()) {
+            groups.put(env.getId(), groupDAO.load(env.getId()));
+        }
         
         header(n("QueryAlertsandSilences"));
         queryalerts(queryalerts);
@@ -46,13 +57,14 @@ public class QueryalertsAndSilencesPage extends HPage {
         for (PrometheusResult r : queryalerts) {
             var m = list.add();
             m.put("name", esc(r.getAlertName()));
+            m.put("link", esc(link(r.getAlertName())));
             m.put("state", esc(r.getAlertState()));
             m.put("value", esc(r.getAlertValue()));
             m.put("ts", esc(r.getFormattedTimestamp()));
             m.put("instance", esc(r.getInstance()));
         }
         Cols cols = Cols.of(
-                Col.si("Name", "name"),
+                new Col("Name", "{{if i.link}}<a href=\"{{i.link}}\">{{i.name}}</a>{{else}}{{i.name}}{{/if}}"),
                 Col.si("State", "state"),
                 Col.si("Value", "value"),
                 Col.si("Datum", "ts"),
@@ -60,6 +72,27 @@ public class QueryalertsAndSilencesPage extends HPage {
                 new Col("", "<a href=\"/qas/silence/{{i.name}}\" class=\"btn btn-xs btn-default\">" + n("DoSilence") + "</a>")
                 );
         put("table1", new TableComponent("wauto", cols, model, "queryalerts"));
+    }
+
+    private String link(String alert) {
+        String ret = ""; // Alarmregel nicht gefunden
+        if (!HestiaWebapp.config.isCustomer()) {
+            for (Entry<String, List<AlertGroup>> e : groups.entrySet()) {
+                var pre = "/" + b().getBranch() + "/alert-rule/" + e.getKey() + "/";
+                for (AlertGroup group : e.getValue()) {
+                    for (AlertRule rule : group.getRules()) {
+                        if (rule.getAlert().equalsIgnoreCase(alert)) {
+                            if (ret.isEmpty()) {
+                                ret = pre + group.getId() + "/" + rule.getId() + "/edit";
+                            } else {
+                                return ""; // Alarmregel ist mehrdeutig, dann kein Link liefern
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return ret;
     }
 
     private void silences(List<Silence> silences) {
